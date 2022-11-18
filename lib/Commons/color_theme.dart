8e5a789374cb11f372f2367644cc941f971cc6a7
Rafik <rfkabubakari@gmail.com>
2022-11-18 11:47:35 +0000

    Firebase initialized

--- lib/Commons/color_theme.dart
+++ lib/Commons/color_theme.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xffFF8001);
+const Color primaryColor = Color(0xff028D6E);
 const Color blackColor = Color(0xff040404);
 const Color whiteColor = Color(0xffFFFFFF);
 const Color greenColor = Color(0xff17AD00);
-const Color lightGreyColor = Color(0xffE0E0E0);
+const Color lightGreyColor = Color(0xffA3D3CF);
 const Color blueColor = Color(0xff74A1FF);
-const Color yellowColor = Color(0xffFFD7AE);
-
-
-
-
+const Color yellowColor = Color.fromARGB(255, 206, 245, 242);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:ffeed_hub/Providers/location_provider.dart';
 import 'package:ffeed_hub/app.dart';
+import 'package:ffeed_hub/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(providers: [
     ListenableProvider<LocationProvider>(
         create: (context) => LocationProvider()),
